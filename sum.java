0c4a524be99c5ad073a19ab2e9477071607f1981
lokeshyvs <46455905+lokeshyvs@users.noreply.github.com>
2019-06-15 18:44:14 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -2,11 +2,11 @@ class sum
 {
 public static void main(String[] args)
 {
-int a,b,c;
+int a,c;
 a=500;
 b=1200;
-c=330;
+c=330
 c=a+b+c;
-System.out.println("The sum of  three values are "+c);
+ystem.out.println("The sum of  three values are "+c);
 }
 }
